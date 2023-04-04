ac1fb2c80f66103d3b1c9ed3d22d2b5369301286
Baymurat <baymurat.orazov@mail.ru>
2023-04-04 19:30:08 +0500

    feat: implement reset random string

--- src/compoents/login/index.tsx
+++ src/compoents/login/index.tsx
@@ -1,4 +1,4 @@
-import { Box, TextField, Button, FormHelperText, FormControl } from '@mui/material'
+import { Box, TextField, Button, FormHelperText, FormControl, Typography } from '@mui/material'
 import { useState } from 'react'
 
 import { useAuthContext } from '../../context/authContext'
@@ -6,7 +6,7 @@ import { useAuthContext } from '../../context/authContext'
 const LoginPage = () => {
   const [isError, setIsError] = useState<boolean>(false)
   const [password, setPassword] = useState<string>('')
-  const { setAuthorizedState } = useAuthContext()
+  const { setAuthorizedState, setInitializedState } = useAuthContext()
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -29,19 +29,33 @@ const LoginPage = () => {
       component="form"
       onSubmit={handleSubmit}
     >
+      <Typography variant='h5'>
+        Input your password for Login
+      </Typography>
       <TextField
         onChange={({ target }) => setPassword(target.value)}
         value={password}
         fullWidth
         error={isError}
-        label="Input password"
+        label="Password"
         type='password'
       />
       {isError && <FormControl error={isError} variant='outlined'>
         <FormHelperText id="component-error-text">Incorrect Password</FormHelperText>
       </FormControl>}
       <Button fullWidth type='submit' variant='contained' color='success'>
-        Submit
+        Login
+      </Button>
+      or
+      <Button
+        fullWidth
+        variant='contained'
+        color='warning'
+        onClick={() => {
+          setInitializedState(null)
+        }}
+      >
+        Reset random string
       </Button>
     </Box>
   )
